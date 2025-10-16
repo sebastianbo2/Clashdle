467cda8e8ff31a0cb8da5d1c54e636aaedebb033
firedespire <firedespire@gmail.com>
2025-10-16 18:50:05 -0400

    Add data file for troops

---
+++ assets/troops.ts
@@ -0,0 +1,67 @@
+export default `troop_name,category,preferred_target,attack_type,housing_space,movement_speed,attack_speed,range
+Barbarian,Elixir,None,Melee (Ground Only),1,18,1,0.4
+Archer,Elixir,None,Ranged (Ground & Air),1,24,1,3.5
+Giant,Elixir,Defenses,Melee (Ground Only),5,12,2,1
+Goblin,Elixir,Resources,Melee (Ground Only),1,32,1,0.4
+Wall Breaker,Elixir,Walls,Area Splash (Ground Only),2,24,1,1
+Balloon,Elixir,Defenses,Area Splash (Ground Only),5,10,3,0.5
+Wizard,Elixir,None,Area Splash (Ground & Air),4,16,1.5,3
+Healer,Elixir,None,Area Splash (Ground Only),14,16,0.7,5
+Dragon,Elixir,Area Splash (Ground & Air),20,16,1.25,3
+Pekka,Elixir,None,Melee (Ground Only),25,16,1.8,0.8
+Baby Dragon,Elixir / Builder Base,None,Area Splash (Ground & Air),10,20,1,2.75
+Miner,Elixir,None,Single Target,6,32,1.7,0.5
+Electro Dragon,Elixir,None,Ranged (Ground & Air),30,13,3.5,3
+Yeti,Elixir,None,Melee (Ground Only),18,12,1,0.8
+Dragon Rider,Elixir,Defenses,Single Target,25,20,1.2,4
+Electro Titan,Elixir,None,Ranged (Ground & Air),32,16,1.5,1.25
+Root Rider,Elixir,Defenses,Single Target,20,12,2.2,1
+Thrower,Elixir,None,Single Target,16,16,2.5,6
+Minion,Dark Elixir,None,Ranged (Ground & Air),2,32,1,2.75
+Hog Rider,Dark Elixir,Defenses,Melee (Ground Only),5,24,1,0.6
+Valkyrie,Dark Elixir,None,Area Splash (Ground Only),8,24,1.8,0.5
+Golem,Dark Elixir,Defenses,Melee (Ground Only),30,12,2.4,1
+Witch,Dark Elixir,None,Area Splash (Ground & Air),12,12,0.7,4
+Lava Hound,Dark Elixir,Air Defense,Melee (Ground Only),30,20,2,1
+Bowler,Dark Elixir,None,Area Splash (Ground Only),6,14,2.2,3
+Ice Golem,Dark Elixir,Defenses,Melee (Ground Only),15,12,2,1
+Headhunter,Dark Elixir,Heroes,Single Target,6,32,0.6,3
+Apprentice Warden,Dark Elixir,None,Ranged (Ground & Air),20,20,0.9,5
+Druid,Dark Elixir,None,Ranged (Ground & Air),16,24,1,5
+Furnace,Dark Elixir,-,-,-,0,-,-
+Super Barbarian,Super Troop / Clan Capital,None,Melee (Ground Only),5,20,0.8,0.6
+Super Archer,Super Troop,None,Ranged (Ground & Air),12,24,1.5,6
+Super Giant,Super Troop / Clan Capital,Defenses,Melee (Ground Only),10,12,2,1
+Sneaky Goblin,Super Troop,Resources,Melee (Ground Only),3,32,1,0.4
+Super Wall Breaker,Super Troop,Walls,Area Splash (Ground Only),8,28,0.6
+Rocket Balloon,Super Troop / Clan Capital,Area Splash (Ground Only),8,12,3,0.5
+Super Wizard,Super Troop / Clan Capital,None,Single Target,10,20,2,3
+Super Dragon,Super Troop / Clan Capital,None,Area Splash (Ground & Air),40,14,1.8,3
+Inferno Dragon,Super Troop / Clan Capital,None,Single Target,15,18,0.128,4
+Super Minion,Super Troop,None,Single Target,12,16,1,4
+Super Valkyrie,Super Troop,None,Area Splash (Ground Only),20,24,1.1,0.6
+Super Witch,Super Troop,None,Area Splash (Ground & Air),40,12,0.7,4
+Ice Hound,Super Troop,Air Defense,Melee (Ground Only),40,20,2,0.75
+Super Bowler,Super Troop,None,Area Splash (Ground Only),30,14,2.2,3
+Super Miner,Super Troop / Clan Capital,None,Single Target,24,32,0.25,0.6
+Super Hog Rider,Super Troop,Defenses,Melee (Ground Only),12,24,1,0.6
+Super Yeti,Super Troop,None,Melee (Ground Only),35,12,1,0.8
+Raged Barbarian,Builder Base,None,Melee (Ground Only),4,19,0.8,0.4
+Sneaky Archer,Builder Base / Clan Capital,None,Ranged (Ground & Air),3,29,1,3.5
+Boxer Giant,Builder Base,Defenses,Single Target,18,14,2,1
+Beta Minion,Builder Base,None,Ranged (Ground & Air),-,38,1,4
+Bomber,Builder Base,Walls,Area Splash (Ground Only),12,19,2,3.5
+Cannon Cart,Builder Base,None,Ranged (Ground Only),16,19,1.2,4.5
+Night Witch,Builder Base,None,Area Splash (Ground & Air),14,12,0.7,4
+Drop Ship,Builder Base,Defenses,Area Splash (Ground Only),-,12,3.5,0.5
+Power P.E.K.K.A,Builder Base / Clan Capital,None,Single Target,22,16,1.8,0.8
+Hog Glider,Builder Base / Clan Capital,Defenses,Melee (Ground Only),12,24,1,0.75
+Electrofire Wizard,Builder Base,None,Ranged (Ground & Air),14,22,0.128,3.5
+Battle Ram,Clan Capital,Walls,Area Splash (Ground Only),5,16,1,0.75
+Minion Horde,Clan Capital,None,Ranged (Ground & Air),2,24,1,1.5
+Skeleton Barrel,Clan Capital,Defenses,Melee (Ground Only),5,8,1,0.5
+Flying Fortress,Clan Capital,Defenses,Ranged (Ground & Air),100,9,2,4.5
+Raid Cart,Clan Capital,None,Ranged (Ground Only),30,9,2.5,4.5
+Mountain Golem,Clan Capital,None,Area Splash (Ground Only),160,6,3,2
+Mega Sparky,Clan Capital,Defenses,Area Splash (Ground Only),100,8,4,4
+`
